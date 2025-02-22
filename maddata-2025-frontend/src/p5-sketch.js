4f09df257d22f8755821915199349193a27047ff
Rochelle Li <rochelleli@Rochelles-MacBook-Pro.local>
2025-02-22 10:41:10 -0600

    fixed p5 wrapper code

--- maddata-2025-frontend/src/p5-sketch.js
+++ maddata-2025-frontend/src/p5-sketch.js
@@ -1,17 +1,22 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 const P5Wrapper = () => {
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "./script.js"; // Ensure this path is correct
     script.async = true;
+    script.onload = () => {
+      console.log("Script loaded");
+    };
     document.body.appendChild(script);
 
     return () => {
       document.body.removeChild(script); // Cleanup script on unmount
     };
-  }, []);
 
+    
+
+  }, []);
   return <div id="p5-container"></div>;
 };
 
