6ea080ca98f69268e4f407ca1e47028724b8f5c8
Anuj Gali <anujgali@gmail.com>
2025-02-23 10:16:16 -0600

    Forgot to commit App.js on previous commit.

--- maddata-2025-frontend/src/App.js
+++ maddata-2025-frontend/src/App.js
@@ -1,19 +1,33 @@
-import './App.css';
-import ChatModal from './chat-modal';
-import P5Wrapper from './p5-sketch';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import ChatModal from "./chat-modal";
+import "./App.css";
+
 function App() {
   return (
-    
-    <div className="App">
-      <header>
-        <h1>MadPictures</h1>
-      </header>
-      <ChatModal></ChatModal>
-      <footer>
-        <p>MadPictures can make mistakes. Check important info</p>
-      </footer>
-    </div>
+    <Router>
+      <Routes>
+        
+        <Route path="/" element={<HomePage />} />
+
+        <Route 
+          path="/chat" 
+          element={
+            <div className="App">
+              <header>
+                <h1>MadPictures</h1>
+              </header>
+              <ChatModal />
+              <footer>
+                <p>MadPictures can make mistakes. Check important info</p>
+              </footer>
+            </div>
+          } 
+        />
+      </Routes>
+    </Router>
   );
-}
+} 
 
 export default App;
